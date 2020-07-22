33ea27b5e51e2655ef1925d0e44cf97e4ab6a1b0
Obiora Igboanusi <cwertlinks@gmail.com>
2020-07-22 01:05:05 +0100

    programmed localstorage functionality

--- scripts/app.js
+++ scripts/app.js
@@ -1,17 +1,82 @@
 "use strict";
 
-let breakfastType = {
-  foodName: "beans",
-  foodTime: 7
+let details = {
+  breakfast: { food_Name: "", food_Time: "" },
+  lunch: { food_Name: "", food_Time: "" },
+  dinner: { food_Name: "", food_Time: "" }
 };
-let lunchType = {
-  foodName: "beans",
-  foodTime: 12
+let dataItem = localStorage.getItem('details');
+const detailsKey = localStorage.key('details');
+let data = JSON.parse(dataItem);
+
+const foodName = document.getElementById('meal-name');
+const foodTime = document.getElementById('meal-time');
+const foodType = document.getElementById('meal-type');
+const button = document.querySelector('#add-item');
+const tableBody = document.querySelector('tbody');
+
+window.addEventListener('load', () => {
+  if (dataItem) {
+    console.log('yes');
+    loadDetails();
+  } else {
+    console.log('no');
+    localStorage.setItem('details', JSON.stringify(details))
+  };
+});
+
+
+
+button.addEventListener('click', function () {
+  let fTime = foodTime.value;
+  let fName = foodName.value;
+  let fType = foodType.value;
+  if (fTime && fName) {
+    if (fType == 'breakfast') {
+      console.log(fType, fName, fTime);
+      data.breakfast.food_Name = fName;
+      data.breakfast.food_Time = fTime;
+      localStorage.setItem("details",JSON.stringify(data));
+      updateContent();
+    } else if (fType == 'lunch') {
+      console.log(fType, fName, fTime);
+      data.lunch.food_Name = fName;
+      data.lunch.food_Time = fTime;
+      localStorage.setItem("details",JSON.stringify(data));
+      updateContent();
+    } else {
+      console.log(fType, fName, fTime);
+      data.dinner.food_Name = fName;
+      data.dinner.food_Time = fTime;
+      localStorage.setItem("details", JSON.stringify(data));
+      updateContent();
+    };
+    foodTime.value = "";
+    foodName.value = "";
+  };
+})
+// functions declaration
+function loadDetails() {
+  if (data.breakfast.food_Name != "") {
+    addRow('breakfast', data.breakfast.food_Name, data.breakfast.food_Time);
+  };
+  if (data.lunch.food_Name != "") {
+    addRow('lunch', data.lunch.food_Name, data.lunch.food_Time);
+  };
+  if (data.dinner.food_Name != "") {
+    addRow('dinner', data.dinner.food_Name, data.dinner.food_Time);
+  }
 };
-let dinnerType = {
-  foodName: "beans",
-  foodTime: 6
+
+function addRow(fType, fName, fTime) {
+  const row = `<tr><td class="foodtype"> ${fType} </td><td>${fName}</td><td>${fTime}</td></tr>`;
+  
+  tableBody.insertAdjacentHTML('beforeend', row);
 };
+function updateContent() {
+  tableBody.innerHTML = '';
+  loadDetails();
+}
 
 function clock() {
   let date = new Date();
@@ -46,29 +111,70 @@ function clock() {
   const photoDisplay = document.querySelector('#photo-container');
 
   switch (hours) {
-    case breakfastType.foodTime:
+    case data.breakfast.food_Time:
       mealDisplay.innerText = "Breakfast";
       foodDisplay.innerText = breakfastType.foodName;
-      photoDisplay.style.backgroundImage = "url('/images/pic01.jpg')";
-    case lunchType.foodTime:
+      photoDisplay.style.backgroundImage = "url('./images/pic01.jpg')";
+    case data.lunch.food_Time:
       mealDisplay.innerText = "Lunch";
       foodDisplay.innerText = lunchType.foodName;
-      photoDisplay.style.backgroundImage = "url('/images/pic02.jpg')";
-    case dinnerType.foodTime:
+      photoDisplay.style.backgroundImage = "url('./images/pic02.jpg')";
+    case data.dinner.food_Time:
       mealDisplay.innerText = "Dinner";
       foodDisplay.innerText = dinnerType.foodName;
-      photoDisplay.style.backgroundImage = "url('/images/pic03.jpg')";
+      photoDisplay.style.backgroundImage = "url('./images/pic03.jpg')";
     default:
       mealDisplay.innerText = "Coding";
       foodDisplay.innerText = "coding";
-      photoDisplay.style.backgroundImage = "url('/images/pic02.jpg')";
+      photoDisplay.style.backgroundImage = "url('./images/pic02.jpg')";
   };
-
 
   //update time
   let update = setTimeout(clock, 500);
 }
 clock();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // track the form
 const form = document.querySelector('#form');
